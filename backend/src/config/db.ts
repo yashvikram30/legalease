5715150c5a5f4a8e4483a388f3e63ed24fd872e2
Yash Vikram <143057359+yashvikram30@users.noreply.github.com>
2025-07-27 12:25:39 +0530

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(process.env.MONGO_URL as string);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
